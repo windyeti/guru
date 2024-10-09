a17e4a6dac02c10498a251cda01c60a8bee2cbf0
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2024-10-09 20:25:47 +0300

    add scope & validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
 class Answer < ApplicationRecord
-  belongs_to :question
+  MAX_ANSWERS_OF_QUESTION = 4
+
+  belongs_to :question, inverse_of: :answers
+
+  scope :correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validate :validate_count_answers_question, on: :create
+
+  def validate_count_answers_question
+    count = question.answers.count
+    errors.add(:count_answers_question, "Count answers must be in 1..4") if count >= MAX_ANSWERS_OF_QUESTION
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :test
+
+  has_many :ansewrs, dependent: :destroy, invere_of: :question
+
+  validetes :body, presence: true
+
 end
